18d0c31170b3d76e770a1d1ed3e772e5739922c3
ABDERRAHIMALOUGANE <83878728+ABDERRAHIMALOUGANE@users.noreply.github.com>
2021-06-08 01:14:45 +0100

    Add files via upload

---
+++ Facebook_page/script.js
@@ -0,0 +1,11 @@
+
+let btn=document.querySelector(".login_btn")
+
+
+btn.addEventListener("click",function(){
+
+    alertify.alert("You have been haked  send me 1000 dollar or i gonna kill you!!!!!!!!")
+ 
+})
+
+
